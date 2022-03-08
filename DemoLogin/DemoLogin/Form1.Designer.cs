9093e1f312fcfdf86ad7047bd679959979084945
Duc <meowd3110@gmail.com>
2022-03-09 01:18:43 +0700

    commit

--- DemoLogin/DemoLogin/Form1.Designer.cs
+++ DemoLogin/DemoLogin/Form1.Designer.cs
@@ -35,14 +35,16 @@ namespace DemoLogin
             this.label3 = new System.Windows.Forms.Label();
             this.Username = new System.Windows.Forms.TextBox();
             this.Password = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.Location = new System.Drawing.Point(308, 243);
+            this.button1.Location = new System.Drawing.Point(231, 197);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 56);
+            this.button1.Size = new System.Drawing.Size(151, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "Đăng Nhập";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,9 +54,10 @@ namespace DemoLogin
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(150, 119);
+            this.label1.Location = new System.Drawing.Point(112, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.Size = new System.Drawing.Size(85, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "UserName";
             // 
@@ -62,9 +65,10 @@ namespace DemoLogin
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(150, 172);
+            this.label2.Location = new System.Drawing.Point(112, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "PassWord";
             // 
@@ -72,41 +76,55 @@ namespace DemoLogin
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(343, 27);
+            this.label3.Location = new System.Drawing.Point(257, 22);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 44);
+            this.label3.Size = new System.Drawing.Size(95, 36);
             this.label3.TabIndex = 3;
             this.label3.Text = "Login";
             // 
             // Username
             // 
             this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Username.Location = new System.Drawing.Point(298, 118);
+            this.Username.Location = new System.Drawing.Point(224, 96);
+            this.Username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(282, 28);
+            this.Username.Size = new System.Drawing.Size(212, 24);
             this.Username.TabIndex = 4;
             this.Username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Password
             // 
             this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Password.Location = new System.Drawing.Point(298, 171);
+            this.Password.Location = new System.Drawing.Point(224, 139);
+            this.Password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Password.Name = "Password";
             this.Password.PasswordChar = '*';
-            this.Password.Size = new System.Drawing.Size(282, 28);
+            this.Password.Size = new System.Drawing.Size(212, 24);
             this.Password.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 355);
+            this.ClientSize = new System.Drawing.Size(600, 288);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.Username);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -122,6 +140,7 @@ namespace DemoLogin
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox Username;
         private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button button2;
     }
 }
 
